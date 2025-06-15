ba13e391fc7d58b2ef69047d6e72cb6d11b2c7a1
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-15 12:33:55 +0700

    initFirst

---
+++ test_dart_instalasi/bin/test_dart_instalasi.dart
@@ -0,0 +1,5 @@
+import 'package:test_dart_instalasi/test_dart_instalasi.dart' as test_dart_instalasi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_dart_instalasi.calculate()}!');
+}
---
+++ test_dart_instalasi/test/test_dart_instalasi_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_dart_instalasi/test_dart_instalasi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
