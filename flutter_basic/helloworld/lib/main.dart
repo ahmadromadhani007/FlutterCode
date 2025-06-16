84b89e21ca5d50143262fdff421305509c4be64e
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-16 15:42:24 +0700

    textoverflow

--- flutter_basic/helloworld/lib/main.dart
+++ flutter_basic/helloworld/lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(new MyApp());
-} 
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Selamat Datang Dunia tifu2"),),
-        body: Center(child: Text("Selamat Datang di Dunia tifu genk")),
+        appBar: AppBar(title: Text('Aplikasi HelloWorld')),
+        body: Center(
+          child: Container(
+            color: Colors.lightBlue,
+            width: 150,
+            height: 50,
+            child: Text("Selamat Datang di Dunia tifu mwhehhehe", maxLines: 2, overflow: TextOverflow.ellipsis,),
+          ),
+        ),
       ),
     );
   }
