6e76ec150ff2ec4d2359a36774f5f2435f017004
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-18 21:49:01 +0700

    train_containers

--- flutter_basic/latihan_container/lib/main.dart
+++ flutter_basic/latihan_container/lib/main.dart
@@ -8,9 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Container Training',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
       home: Scaffold(
-        appBar: AppBar(title: Text("Latihan Container")),
-        body: Container(color: Colors.red, margin: EdgeInsets.all(10)),
+        appBar: AppBar(title: Text("Container Training")),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+          padding: EdgeInsets.only(bottom: 20, top: 20),
+          child: Container(
+            // color: Colors.blueAccent,
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[Colors.amberAccent, Colors.lightBlueAccent],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
