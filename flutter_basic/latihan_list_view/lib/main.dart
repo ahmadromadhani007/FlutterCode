fb886d215ef123798400a05fb2c9586d2d8cc27f
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-22 12:30:43 +0700

    listviewww

--- flutter_basic/latihan_list_view/lib/main.dart
+++ flutter_basic/latihan_list_view/lib/main.dart
@@ -13,18 +13,37 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  List<Widget> widgets = [];
+
+  _MyAppState() {
+    for (int i = 0; i < 15; i++) widgets.add(Text("Data ke-" + i.toString(), style: TextStyle(fontSize: 30),));
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Latihan List & View',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Latihan list and view demo')),
-        
+        appBar: AppBar(title: const Text('Latihan list and views')),
+        body: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(child: Text("Tambah Data"), onPressed: null,),
+                ElevatedButton(child: Text("Hapus Data"), onPressed: null,),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widgets,
+            )
+          ]
+          ),
       ),
     );
   }
