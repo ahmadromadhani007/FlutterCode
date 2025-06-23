fcb4af518f2fc8734e16e8fffa3e5e0b8fd22f1b
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-23 19:54:52 +0700

    animatedContainers

--- flutter_basic/latihan_animated_container/lib/main.dart
+++ flutter_basic/latihan_animated_container/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +13,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Random random = Random();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
-    }
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Latihan Animated Container')),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(
+                255,
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+              ),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
