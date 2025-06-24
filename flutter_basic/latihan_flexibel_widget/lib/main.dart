3d34e966da0c4471bcc2c1d7dca93323f00ff4a2
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-24 22:07:12 +0700

    flexibelwidget

--- flutter_basic/latihan_flexibel_widget/lib/main.dart
+++ flutter_basic/latihan_flexibel_widget/lib/main.dart
@@ -3,3 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Latihan Flexible Widget')),
+        body: ListView(
+          
+        ),
+      ),
+    );
+  }
+}
