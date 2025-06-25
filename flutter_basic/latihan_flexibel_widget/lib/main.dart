cec1a04543b84ba8fa9b69772b02b6d0c24d0c80
Ahmadromadhanny <dan10kiy@gmail.com>
2025-06-25 21:18:51 +0700

    flexibelwidgets

--- flutter_basic/latihan_flexibel_widget/lib/main.dart
+++ flutter_basic/latihan_flexibel_widget/lib/main.dart
@@ -4,27 +4,72 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
         ),
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('Latihan Flexible Widget')),
-        body: ListView(
-          
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      color: Colors.green,
+                    ),
+                  ),
+                                    Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      color: Colors.brown,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                color: Colors.red,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                color: Colors.amber,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                color: Colors.blue,
+              ),
+            ),
+          ],
         ),
       ),
     );
